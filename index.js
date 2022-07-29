95244fc8df411c414c8c59411a8d4b3856865508
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-07-29 12:53:06 -0500

    feat: awesome users

---
+++ index.js
@@ -0,0 +1,12 @@
+const axios = require('axios')
+
+const greet = name => 'Hello' + name
+const users = async() => {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+    return res.data
+}
+
+module.exports = {
+    greet,
+    users
+}
